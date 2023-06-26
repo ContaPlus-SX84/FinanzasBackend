41859dd1b995580fde9816a9e5cadb9258c04b84
Caleb <reateguicevallosc@gmail.com>
2023-06-26 12:10:51 -0500

    Delete: French Method and Gastos Models

--- src/main/java/com/finanzas/finanzasbackend/finanzas/mapping/MappingConfiguration.java
+++ src/main/java/com/finanzas/finanzasbackend/finanzas/mapping/MappingConfiguration.java
@@ -1,7 +1,5 @@
 package com.finanzas.finanzasbackend.finanzas.mapping;
 
-import com.finanzas.finanzasbackend.finanzas.mapping.frenchMethod.FrenchMethodMapper;
-import com.finanzas.finanzasbackend.finanzas.mapping.gastos.GastosMapper;
 import com.finanzas.finanzasbackend.finanzas.mapping.user.UserMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +10,4 @@ import java.io.Serializable;
 public class MappingConfiguration implements Serializable {
     @Bean
     public UserMapper userMapper() { return new UserMapper(); }
-
-    @Bean
-    public FrenchMethodMapper frenchMethodMapper() { return new FrenchMethodMapper(); }
-
-    @Bean
-    public GastosMapper gastosMapper() { return new GastosMapper(); }
-
 }
